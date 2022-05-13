3ce74b433aef00546621a021fe191aa94a83f23a
余弦 <yuxian158@gmail.com>
2022-05-13 11:33:21 +0800

    test

--- ranran/DIY/download.py
+++ ranran/DIY/download.py
@@ -21,27 +21,5 @@ async def handler(event):
     end_time = time.time()
     # await conv.send_message(f'下载完成文件名{filename}')
     await ranran.send_message(my_chat_id,f'下载完成路径为http://alist.yuxian158.work/local/{timess}.mp4')
-    await ranran.send_message(my_chat_id,f'用时{round(end_time - start)}s')
+    await ranran.send_message(my_chat_id,f'用pip freeze > ./requirements.txt  时{round(end_time - start)}s')
 
-    # user_id = event.sender_id
-    # async with client.conversation(user_id) as conv:
-    #     if event.raw_text == "删除":
-    #         await conv.send_message('确定删除所有视频吗？')
-    #         response = await conv.get_response()
-    #         if response.text == "是":
-    #             os.system("rm -rf /aaa/files/video/*")
-    #             await conv.send_message('成功')
-    #         else:
-    #             await conv.send_message('退出')
-    #     if event.media is not None:
-    #         await conv.send_message('检测到视频开始下载')
-            # paths = await event.download_media()
-            # timess = time.strftime("%m%d%H%M%S", time.localtime())
-            # filename = path + timess
-            # start = time.time()
-            # await client.download_media(event.media, filename)
-            # end_time = time.time()
-            # # await conv.send_message(f'下载完成文件名{filename}')
-            # await conv.send_message(f'下载完成路径为http://alist.yuxian158.work/local/{timess}.mp4')
-            # await conv.send_message(f'用时{round(end_time - start)}s')
-    #     print(event.raw_text)
