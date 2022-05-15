525f9566dfa5da84bd07cad0db5a4b8458a519ce
余弦 <yuxian158@gmail.com>
2022-05-15 19:20:54 +0800

    add gitignore

---
+++ ranran/DIY/send_one.py
@@ -0,0 +1,16 @@
+import asyncio
+
+import aiocron
+import aiohttp
+
+from .. import ranran,my_chat_id,logger
+
+
+@aiocron.crontab('* */6 * * *')
+async def one():
+    async with aiohttp.ClientSession() as session:
+        async with session.get("https://v1.hitokoto.cn") as response:
+            js = await response.json()
+            xt=js.get("hitokoto") + "  --" + js.get("from")
+            await ranran.send_message(my_chat_id,xt)
+        logger.info(f"向{my_chat_id}发送{xt}")
